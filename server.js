8942c63705aef60df9a776270eefe90deb1c4056
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-10-23 16:36:35 +0300

    more factory controller stuff + initial server requests

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost/workoutBuddy');
+mongoose.connect('mongodb://localhost/workoutBuddy');
 
-// var Workout = require("./workoutModel");
+var Workout = require("./model");
 
 var app = express();
 
@@ -14,19 +14,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-var handler = function(res, next){
-    return function(err, workout) {
-      if (err) {
-        return next(err);
-      }
-      res.send(workout);
+var handler = function (res, next) {
+  return function (err, workout) {
+    if (err) {
+      return next(err);
     }
+    res.send(workout);
   }
+}
 
+app.post('/post-user', function(request, response, next){
+  Workout.create(request.body, handler(response, next));
+});
 
+app.all('*', function (req, res) {
+  res.sendFile(__dirname + "/public/index.html")
+})
 
 // error handler to catch 404 and forward to main error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -34,7 +40,7 @@ app.use(function(req, res, next) {
 
 // main error handler
 // warning - not for use in production code!
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.send({
     message: err.message,
@@ -42,12 +48,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
-app.all('*', function(req, res) {
-  res.sendFile(__dirname  + "/public/index.html")
-})
-  
-  
-  app.listen(8000, function() {
-    console.log("yo yo yo, on 8000!!")
-  });
+app.listen(8000, function () {
+  console.log("yo yo yo, on 8000!!")
+});
