f9a80ab7ffb23ac98583dd768338ffcde5b378db
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-10-23 14:22:37 +0300

    factory dummy data, controller-factory get sync, input date

--- public/controller.js
+++ public/controller.js
@@ -1,14 +1,17 @@
 app.controller('mainCtrl', ['$scope', 'mainFactory', function($scope, mainFactory){
     // Stats
-    $scope.successionCounter = 3;
+    $scope.successionCounter = mainFactory.dummyData.currentStreakCounter;
     // Goals
-    $scope.goal = 1 + ' times per ' + 'week';
+    $scope.goal = mainFactory.dummyData.settings.goalFrequency + ' times per ' + mainFactory.dummyData.settings.goalIntervalSetting;
     // Progress
-    $scope.progress = 5 / 25 * 100 + "%";
+    $scope.progress = mainFactory.dummyData.savings / mainFactory.dummyData.settings.targetGiftPrice * 100 + "%";
     // Motivational Quote
-    $scope.quote = ["keep it up!", "everyday counts!"][1];
+    $scope.quote = mainFactory.quotes[Math.floor(Math.random() * mainFactory.quotes.length)];
     // Date & Time
-
+    var date = new Date();
+    date.setMilliseconds(0);
+    date.setSeconds(0);
+    $scope.date = date;
     // WorkOut Button
     $scope.workedOut = function(){
         alert("I worked out!")
--- public/factory.js
+++ public/factory.js
@@ -1,36 +1,61 @@
 app.factory('mainFactory', [function () {
-    var quotes = {
-        quotes: ["keep it up!", "everyday counts!"]
-    }
-    var user = {
-        name: name,
-        image: image_url,
-        savings: savings,
-        currentStreakCounter: currentStreakCounter,
-        allTimeBest: { type: Number },
+    var workoutObj = {};
+
+    workoutObj.dummyData = {
+        _id: 12345,
+        name: "Shy",
+        image_url: "https://vignette3.wikia.nocookie.net/nintendo/images/e/e1/ShyGuy.PNG/revision/latest?cb=20110929231546&path-prefix=en",
+        savings: 5,
+        currentStreakCounter: 2,
+        allTimeBest: 4,
         settings: {
-            workoutCost: workoutCost,
-            goalFrequency: goalFrequency,
-            goalIntervalSetting: goalIntervalSetting,
-            streakTarget: streakTarget,
-            bonusAmt: bonusAmt,
-            targetGiftPrice: targetGiftPrice
-        }
+            workoutCost: 1,
+            goalFrequency: 1,
+            goalIntervalSetting: "week",
+            streakTarget: 3,
+            bonusAmt: 2,
+            targetGiftPrice: 30
+        },
+        logs: [
+            {
+                currentStreakCounter: 2,
+                savings: 5,
+                workoutCost: 1,
+                date: new Date(),
+                bonusStreakAdded: 0
+            }]
     }
 
-    var log = {
-    currentStreakCounter: currentStreakCounter,
-    savings: user.savings + user.settings.workoutCost,
-    workoutCost: user.settings.workoutCost,
-    date: date,
-    bonusStreakAdded: function () {
-        if (user.currentStreakCounter === user.settings.streakTarget) {
-            return settings.bonusAmt
-        }
-        return 0;
-    }
-}
+    workoutObj.quotes = ["keep it up!", "everyday counts!"];
+    // var user = {
+    //     name: name,
+    //     image: image_url,
+    //     savings: savings,
+    //     currentStreakCounter: currentStreakCounter,
+    //     allTimeBest: { type: Number },
+    //     settings: {
+    //         workoutCost: workoutCost,
+    //         goalFrequency: goalFrequency,
+    //         goalIntervalSetting: goalIntervalSetting,
+    //         streakTarget: streakTarget,
+    //         bonusAmt: bonusAmt,
+    //         targetGiftPrice: targetGiftPrice
+    //     }
+    // }
 
-return quotes, user, log;
+    // var log = {
+    //     currentStreakCounter: currentStreakCounter,
+    //     savings: user.savings + user.settings.workoutCost,
+    //     workoutCost: user.settings.workoutCost,
+    //     date: date,
+    //     bonusStreakAdded: function () {
+    //         if (user.currentStreakCounter === user.settings.streakTarget) {
+    //             return settings.bonusAmt
+    //         }
+    //         return 0;
+    //     }
+    // }
+
+    return workoutObj;
 
 }]);
