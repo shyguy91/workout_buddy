ad6de63e1d3c3681c738867ba2adad666f334041
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-10-23 11:46:43 +0300

    fixed yitz's factory syntax

--- public/factory.js
+++ public/factory.js
@@ -1,7 +1,7 @@
 app.factory('mainFactory', [function () {
     var quotes = {
-      quotes:  ["keep it up!", "everyday counts!"]
-    },
+        quotes: ["keep it up!", "everyday counts!"]
+    }
     var user = {
         name: name,
         image: image_url,
@@ -14,21 +14,23 @@ app.factory('mainFactory', [function () {
             goalIntervalSetting: goalIntervalSetting,
             streakTarget: streakTarget,
             bonusAmt: bonusAmt,
-            targetGiftPrice: targetGiftPrice}
-        }
-    },
-    var log = {
-        currentStreakCounter: currentStreakCounter,
-        savings = user.savings + user.settings.workoutCost,
-        workoutCost = user.settings.workoutCost,
-        date,
-        bonusStreakAdded = function (){
-        if (user.currentStreakCounter === user.settings.streakTarget){
-                return settings.bonusAmt
-        }
-                return 0;
+            targetGiftPrice: targetGiftPrice
         }
     }
 
-        return quotes, user, log;
+    var log = {
+    currentStreakCounter: currentStreakCounter,
+    savings: user.savings + user.settings.workoutCost,
+    workoutCost: user.settings.workoutCost,
+    date: date,
+    bonusStreakAdded: function () {
+        if (user.currentStreakCounter === user.settings.streakTarget) {
+            return settings.bonusAmt
+        }
+        return 0;
+    }
+}
+
+return quotes, user, log;
+
 }]);
