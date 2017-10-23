9fcbec2d8f62ad82619c3df1eeb38a1f360f84ce
YZeitler <amz61@msn.com>
2017-10-23 14:13:32 +0300

    bootstrapped log.html

--- public/factory.js
+++ public/factory.js
@@ -1,39 +1,40 @@
 app.factory('mainFactory', [function () {
-    var bonusHit = false;
+//     var bonusHit = false;
     
-    var quotes = {
-        quotes: ["keep it up!", "everyday counts!"]
-    }
-    var user = {
-        name: name,
-        image: image_url,
-        savings: savings,
-        currentStreakCounter: currentStreakCounter,
-        allTimeBest: { type: Number },
-        settings: {
-            workoutCost: workoutCost,
-            goalFrequency: goalFrequency,
-            goalIntervalSetting: goalIntervalSetting,
-            streakTarget: streakTarget,
-            bonusAmt: bonusAmt,
-            targetGiftPrice: targetGiftPrice
-        }
-    }
+//     var quotes = {
+//         quotes: ["keep it up!", "everyday counts!"]
+//     }
+//     var user = {
+//         name: name,
+//         image: image_url,
+//         savings: savings,
+//         currentStreakCounter: currentStreakCounter,
+//         allTimeBest: { type: Number },
+//         settings: {
+//             workoutCost: workoutCost,
+//             goalFrequency: goalFrequency,
+//             goalIntervalSetting: goalIntervalSetting,
+//             streakTarget: streakTarget,
+//             bonusAmt: bonusAmt,
+//             targetGiftPrice: targetGiftPrice
+//         }
+//     }
 
-    var log = {
-    currentStreakCounter: currentStreakCounter,
-    savings: user.savings + user.settings.workoutCost,
-    workoutCost: user.settings.workoutCost,
-    date: date,
-    bonusStreakAdded: function () {
-        if (user.currentStreakCounter === user.settings.streakTarget) {
-            bonusHit = true;
-            return settings.bonusAmt
-        }
-        return 0;
-    }
-}
+//     var log = {
+//     currentStreakCounter: currentStreakCounter,
+//     savings: user.savings + user.settings.workoutCost,
+//     workoutCost: user.settings.workoutCost,
+//     date: date,
+//     bonusStreakAdded: function () {
+//         if (user.currentStreakCounter === user.settings.streakTarget) {
+//             bonusHit = true;
+//             return settings.bonusAmt
+//         }
+//         return 0;
+//     }
+// }
 
-return quotes, user, log;
+// return quotes, user, log;
 
+return {}
 }]);
