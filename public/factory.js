3a3962d5c591873600b33836274c8a86407d4ae6
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-10-23 10:09:38 +0300

    model fix

--- public/factory.js
+++ public/factory.js
@@ -1,11 +1,13 @@
-app.factory('mainFactory', [function(){
+app.factory('mainFactory', [function () {
     var quotes = ["keep it up!", "everyday counts!"];
-    var user = {streakRecord: 3,
+    var user = {
+        streakRecord: 3,
         goalFrequency: 1,
         goalSetting: 'week',
         targetGiftPrice: 25,
-        savings: 5}
-user.quotes = quotes;
+        savings: 5
+    }
+    user.quotes = quotes;
 
-return user;
+    return user;
 }]);
